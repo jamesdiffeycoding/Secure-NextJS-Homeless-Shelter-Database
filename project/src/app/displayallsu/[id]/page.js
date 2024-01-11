bb99f288be0af22638bca70415985c90644def0a
James <jamesdiffey8@gmail.com>
2024-01-11 07:32:53 +0000

    working blue

--- project/src/app/displayallsu/[id]/page.js
+++ project/src/app/displayallsu/[id]/page.js
@@ -17,12 +17,14 @@ generateStaticParams();
 // AUTHROUTER WITH PROPS -----------------------------------------------------
 export default async function DisplayOneSU({ params }) {
   const id = params.id;
+  let randomNumberForAvoidingCache = Math.floor(Math.random() * 99999999)
   const allFetchedDataAboutSpecificSU = await fetchSpecificSUDataFromSupabase(
-    id
+    id, randomNumberForAvoidingCache
   );
 
   return (
     <>
+      <div>hi {randomNumberForAvoidingCache}</div>
       <AuthRouter
         pageName={"displayonesu"}
         allFetchedDataAboutSpecificSU={allFetchedDataAboutSpecificSU}
