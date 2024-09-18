aff918db0e38a106770c48496ee058848760d897
James Diffey <diffeyj@outlook.com>
2024-09-18 10:48:37 +0100

    update to next 14.2.10 due to cache poisoning vulnerability

--- project/src/app/layout.js
+++ project/src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
   title: 'Shelter App',
   description: 'Helping you to manage your shelter guest information',
   icons: {
-    icon: '/favicon.ico', // /public path
+    icon: '../favicon.ico', // /public path
   }
 }
 
